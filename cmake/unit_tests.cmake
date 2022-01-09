58dac54dd668e9aaccd3c3bfa65f197d2119d3be
Kiril Kirov <cyril.cyrov@gmail.com>
2022-01-09 21:40:15 +0200

    Improve projects structure in VS
    Also introduce add_unit_tests function.

--- cmake/unit_tests.cmake
+++ cmake/unit_tests.cmake
@@ -6,3 +6,13 @@ include(CTest)
 set(CATCH_INCLUDE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/external)
 add_library(Catch2::Catch IMPORTED INTERFACE)
 set_property(TARGET Catch2::Catch PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${CATCH_INCLUDE_DIR}")
+
+function(add_unit_tests project_name)
+    add_subdirectory(tests)
+    
+    # integrate with ctest
+    add_test(NAME ${project_name}_tests COMMAND ${PROJECT_NAME}_tests)
+
+    set_target_properties(${project_name} PROPERTIES FOLDER ${project_name}) 
+    set_target_properties(${project_name}_tests PROPERTIES FOLDER ${project_name})     
+endfunction()
