f126234d09b48bb574129d5e4250f91b96f4de78
Kiril Kirov <cyril.cyrov@gmail.com>
2022-01-22 19:01:43 +0200

    Reorganize scripts + a few cosmetic changes
    Introduce bat/sh wrapper scripts for easier usage.
    Split make.py into Build.py and Run.py.
    Use CamelCase for Python class names.
    Update .gitignore.

---
+++ scripts/Build.py
@@ -0,0 +1,76 @@
+import argparse
+import platform
+import subprocess
+import os
+import sys
+
+class Build:
+    def __init__(self, cmd_args):
+        self._build_dir = 'build'
+        self._generator = 'Ninja'
+        self._extra_gen_args = []
+        self._extra_build_args = []
+        self._verbose = cmd_args.verbose
+        self._tests_target = 'test'
+
+    def build_dir(self):
+        return self._build_dir
+
+    def generate(self):
+        args = ['cmake', '-S', '.', '-B', self._build_dir, '-G', self._generator]
+        args.extend(self._extra_gen_args)
+        self._execute(args)
+
+    def make(self):
+        if not os.path.exists(self._build_dir):
+            self.generate()
+
+        args = ['cmake', '--build', self._build_dir]
+        args.extend(self._extra_build_args)
+        if self._verbose:
+            args.append('-v')
+
+        self._execute(args)
+
+    def test(self):
+        args = ['cmake', '--build', self._build_dir]
+        args.extend(self._extra_build_args)
+        args.extend(['--target', self._tests_target])
+
+        self._execute(args)
+
+    def clean(self):
+        args = ['cmake', '--build', self._build_dir]
+        args.extend(self._extra_build_args)
+        args.extend(['--target', 'clean'])
+
+        self._execute(args)
+
+    def _execute(self, args):
+        subprocess.check_call(args)
+
+class SingleConfigBuild(Build):
+    def __init__(self, cmd_args):
+        super(SingleConfigBuild, self).__init__(cmd_args)
+
+        self._build_dir = str(os.path.join('build', cmd_args.type.lower()))
+        self._extra_gen_args += [f'-DCMAKE_BUILD_TYPE={cmd_args.type}']
+
+class MultiConfigBuild(Build):
+    def __init__(self, cmd_args):
+        super(MultiConfigBuild, self).__init__(cmd_args)
+
+        self._extra_build_args = ['--config', cmd_args.type]
+
+class WindowsBuild(MultiConfigBuild):
+    def __init__(self, cmd_args):
+        super(WindowsBuild, self).__init__(cmd_args)
+        self._generator = 'Visual Studio 16 2019'
+        self._tests_target = 'RUN_TESTS'
+        self._extra_gen_args += ['-A', 'x64']
+
+class LinuxBuild(SingleConfigBuild):
+    def __init__(self, cmd_args):
+        super(LinuxBuild, self).__init__(cmd_args)
+        self._extra_gen_args += ['-DCMAKE_C_COMPILER=clang',
+                                 '-DCMAKE_CXX_COMPILER=clang++']
---
+++ scripts/Run.py
@@ -0,0 +1,59 @@
+import Build
+
+import re
+import os
+import sys
+import platform
+import argparse
+
+def define_arguments():
+    parser = argparse.ArgumentParser(description='Sample wrapper for cmake.')
+
+    command = parser.add_mutually_exclusive_group(required=True)
+    command.add_argument('gen', nargs='?',
+                        help='run cmake configuration')
+    command.add_argument('make', nargs='?',
+                        help='build cmake configuration')
+    command.add_argument('clean', nargs='?',
+                        help='clean cmake configuration')
+    command.add_argument('test', nargs='?',
+                        help='run tests')
+    command.add_argument('all', nargs='?',
+                        help='equivalent to configure, make, run tests')
+
+    parser.add_argument('--type', action='store', default='Debug',
+                        choices=['Debug', 'Release', 'RelWithDebInfo', 'MinSizeRel'],
+                        help='build type')
+    parser.add_argument('--verbose', action='store_const', const=True,
+                        help='verbose')
+
+    return parser
+
+if __name__ == '__main__':
+    operating_system = platform.system()
+    if operating_system not in ['Windows', 'Linux']:
+        sys.exit(f'Unsupported OS: {operating_system}')
+
+    args_definition = define_arguments()
+    cmd_args = args_definition.parse_args()
+
+    builder = Build.WindowsBuild(cmd_args) if operating_system == 'Windows' else Build.LinuxBuild(cmd_args)
+
+    command = cmd_args.gen
+    if (command == 'gen'):
+        builder.generate()
+    elif (command == 'make'):
+        if not os.path.exists(builder.build_dir()):
+            builder.generate()
+        builder.make()
+    elif (command == 'clean'):
+        builder.clean()
+    elif (command == 'test'):
+        builder.test()
+    elif (command == 'all'):
+        builder.generate()
+        builder.make()
+        builder.test()
+    else:
+        args_definition.print_help()
+        args_definition.exit(f'\nUnsupported command: {command}')
