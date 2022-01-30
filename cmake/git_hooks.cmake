fe777290fa75e8df5fa717c3d3736bee8276441d
Kiril Kirov <cyril.cyrov@gmail.com>
2022-01-30 12:59:52 +0200

    Check clang-format version before creating 'format' target

--- cmake/git_hooks.cmake
+++ cmake/git_hooks.cmake
@@ -2,10 +2,11 @@ find_package (Python3 COMPONENTS Interpreter)
 
 if (NOT Python3_FOUND)
     message(WARNING "python3 not found, commit hooks will not be installed")
-else()
-    set(RUN_SCRIPT ${CMAKE_CURRENT_SOURCE_DIR}/scripts/Run.py)
-    configure_file(
-        ${CMAKE_CURRENT_SOURCE_DIR}/scripts/pre-commit-hook
-        ${CMAKE_CURRENT_SOURCE_DIR}/.git/hooks/pre-commit
-        @ONLY)
+    return()
 endif()
+
+set(RUN_SCRIPT ${CMAKE_CURRENT_SOURCE_DIR}/scripts/Run.py)
+configure_file(
+    ${CMAKE_CURRENT_SOURCE_DIR}/scripts/pre-commit-hook
+    ${CMAKE_CURRENT_SOURCE_DIR}/.git/hooks/pre-commit
+    @ONLY)
