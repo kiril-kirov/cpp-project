e0a9c7312460defc26ee4a98e37994fdfb049d0c
Kiril Kirov <cyril.cyrov@gmail.com>
2022-01-30 13:03:48 +0200

    Don't install pre-commit hook if clang-format is too old

--- cmake/git_hooks.cmake
+++ cmake/git_hooks.cmake
@@ -5,6 +5,14 @@ if (NOT Python3_FOUND)
     return()
 endif()
 
+if (NOT CLANG_FORMAT_MAJOR_VERSION)
+    message(WARNING "clang-format version is unknown")
+    return()
+elseif (CLANG_FORMAT_MAJOR_VERSION LESS CLANG_FORMAT_MIN_REQUIRED_VERSION)
+    message(WARNING "clang-format is too old, will not install pre-commit hook (required: ${CLANG_FORMAT_MIN_REQUIRED_VERSION}, installed: ${CLANG_FORMAT_MAJOR_VERSION}")
+    return()
+endif()
+
 set(RUN_SCRIPT ${CMAKE_CURRENT_SOURCE_DIR}/scripts/Run.py)
 configure_file(
     ${CMAKE_CURRENT_SOURCE_DIR}/scripts/pre-commit-hook
